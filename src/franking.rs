93771f1cd3104381cf138fdc704cb107d749a8b2
Matthew Gregoire <mattyg@live.unc.edu>
2024-01-25 10:43:48 -0500

    Outlined project

--- src/franking.rs
+++ src/franking.rs
@@ -1,1 +1,16 @@
 // TODO
+
+/*
+    1. use curve25519 for elgamal implementation
+    2. implement:
+        - Client:
+            * send msg
+            * receive msg
+            * report msg
+        - Moderator:
+            * receive and pass along msg
+        - Server:
+            * receive and pass along msg
+
+        These three are all different in reencryption vs decryption settings
+*/
