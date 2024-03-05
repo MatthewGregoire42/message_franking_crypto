09c8f7fb9c38e309c19a717aec3d544a32e40e92
mapierce23 <126899841+mapierce23@users.noreply.github.com>
2024-03-05 14:08:12 -0500

    Finished, working send_reencrypt()

--- src/franking.rs
+++ src/franking.rs
@@ -1,7 +1,6 @@
 use curve25519_dalek::scalar::Scalar;
 use curve25519_dalek::ristretto::RistrettoPoint;
 use curve25519_dalek::constants as dalek_constants;
-use curve25519_dalek::ristretto::CompressedRistretto;
 use rand::prelude::*;
 use generic_array::GenericArray;
 use hmac::{Hmac, Mac, NewMac};
@@ -10,26 +9,12 @@ use rand_chacha::rand_core::SeedableRng;
 use lazy_static::lazy_static;
 use aes_gcm::{
     aead::{Aead, AeadCore, KeyInit},
-    Aes256Gcm, Nonce, Key
+    Aes256Gcm, Key
 };
 use serde::Deserialize;
 use serde::Serialize;
 use sha2::{Sha256, Digest};
 
-/*
-    1. use curve25519 for elgamal implementation
-    2. implement:
-        - Client:
-            * send msg
-            * receive msg
-            * report msg
-        - Moderator:
-            * receive and pass along msg
-        - Server:
-            * receive and pass along msg
-
-        These three are all different in reencryption vs decryption settings
-*/
 
 lazy_static! {
     pub static ref GEN_G: RistrettoPoint = dalek_constants::RISTRETTO_BASEPOINT_POINT;
@@ -47,18 +32,29 @@ pub struct MessagePackage<'a> {
     pub s: [u8; 32],
 }
 
+#[derive(Serialize, Deserialize)]
+pub struct ModeratorPackage {
+    pub k: Vec<u8>,
+    pub tf: Vec<u8>,
+}
+
 impl Client {
 
-    pub fn send_reencrypt(message: &str, k_r: Key<Aes256Gcm>, pk: RistrettoPoint) -> (RistrettoPoint, Vec<u8>) {
+    pub fn send_reencrypt(message: &str, k_r: Key<Aes256Gcm>, pk: RistrettoPoint, pk_m: RistrettoPoint) -> ((RistrettoPoint, Vec<u8>), (RistrettoPoint, Vec<u8>)) {
 
         // Input: message, public keys, receiver's symmetric key
         // Output: 2 El Gamal Ciphertexts (1 containing encrypted message/franking information, 1 for the moderator)
 
-        // Choose random seed s
+        // Choose random seed s and seed the RNG
         // ====================================================================
         let mut seed: <ChaCha8Rng as SeedableRng>::Seed = Default::default();
         thread_rng().fill(&mut seed);
         let mut rng = ChaCha8Rng::from_seed(seed);
+        // ====================================================================
+
+        // Generate moderator key k
+        // ====================================================================
+        let k = Aes256Gcm::generate_key(&mut rng);
         // ====================================================================
 
         // Generate ephemeral MAC key kf
@@ -70,12 +66,12 @@ impl Client {
         // Compute franking tag tf 
         // ====================================================================
         let mut mac = HmacSha256::new_varkey(&mackey_kf).expect("HMAC");
-        let mut macinput: &[u8] = message.as_bytes();
+        let macinput: &[u8] = message.as_bytes();
         mac.update(macinput);
         let franktag_tf = (mac.finalize()).into_bytes();
         // ====================================================================
 
-        // Encrypt m, kf, tf, s --> ct 
+        // Encrypt m, kf, tf, s --> ct using AES-GCM
         // ====================================================================
         let msg_package = MessagePackage {
             m: message,
@@ -95,31 +91,54 @@ impl Client {
 
         // Encrypt ct under public key into ct' (using Hashed El Gamal)
         // ====================================================================
-        let G: &RistrettoPoint = &GEN_G;
-        let H: &RistrettoPoint = &pk;
+        let g: &RistrettoPoint = &GEN_G;
+        let h: &RistrettoPoint = &pk;
         let mut rng_el = rand::thread_rng();
         let y = Scalar::random(&mut rng_el);
-        let s = H * y;
-        let c1 = G * y;
+        let s = h * y;
+        let c1 = g * y;
         // Rather than multiplying the message with s, we instead hash s and XOR the message
         // with the result. This prevents us from having to map messages to RistrettoPoints 
         // in an invertible way, since that doesn't seem to be doable with this crate (TOOD: is
         // that correct?)
-        let mut hasher = Sha256::new();
-        hasher.update(s.compress().to_bytes());
-        let mut hash_s: Vec<u8> = hasher.finalize().to_vec();
+        let mut hasher1 = Sha256::new();
+        hasher1.update(s.compress().to_bytes());
+        let mut hash_s: Vec<u8> = hasher1.clone().finalize().to_vec();
         let zero_vec = vec![0; ct.len() - 32];
         hash_s.extend(zero_vec);
         let c2: Vec<u8> = ct
-			    .iter()
-			    .zip(hash_s.iter())
-			    .map(|(&x1, &x2)| x1 ^ x2)
-			    .collect();
+                .iter()
+                .zip(hash_s.iter())
+                .map(|(&x1, &x2)| x1 ^ x2)
+                .collect();
         // ====================================================================
-        return (c1, c2);
 
+        // Encrypt k and tf under moderator public key (Hashed El Gamal again)
+        // ====================================================================
+        let h: &RistrettoPoint = &pk_m;
+        let y = Scalar::random(&mut rng_el);
+        let s = h * y;
+        let c1_m = g * y;
+        let mut hasher2 = hasher1.clone();
+        hasher2.update(s.compress().to_bytes());
+        let mut hash_s: Vec<u8> = hasher2.finalize().to_vec();
 
-        // TODO: Encrypt k and tf under moderator public key (el gamal)
+        let mod_package = ModeratorPackage {
+            k: k.to_vec(),
+            tf: franktag_tf.to_vec(),
+        };
+        let mut k_tf: Vec<u8> = Vec::new();
+        k_tf.extend_from_slice(&bincode::serialize(&mod_package).unwrap());
+        let zero_vec = vec![0; k_tf.len() - 32];
+        hash_s.extend(zero_vec);
+        let c2_m: Vec<u8> = k_tf
+                .iter()
+                .zip(hash_s.iter())
+                .map(|(&x1, &x2)| x1 ^ x2)
+                .collect();
+        // ====================================================================
+        return ((c1, c2), (c1_m, c2_m));
+
     }
     pub fn receive_reencrypt(&mut self, message: &str) {
 
