843a42bde48037a8ff59eee32acd74790592a8fb
Matthew Gregoire <mattyg@live.unc.edu>
2024-07-02 10:04:02 -0400

    Moved mods back into src folder

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,6 @@
 #[macro_use]
 
-pub mod franking;
+pub mod lib_general;
+// pub mod lib_trap;
+// pub mod lib_comkey;
+// pub mod lib_optimized;
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 mod lib_general;
-mod lib_trap;
-mod lib_comkey;
-mod lib_optimized;
+// mod lib_trap;
+// mod lib_comkey;
+// mod lib_optimized;
 
 fn main() {
 	println!("Hello, World!");
