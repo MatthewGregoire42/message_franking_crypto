ccac283fa4bf8f4531dd59331fccf3a8c49a4e34
Matthew Gregoire <mattyg@live.unc.edu>
2024-07-06 11:15:44 -0400

    Added time tracking

--- src/main.rs
+++ src/main.rs
@@ -448,129 +448,54 @@ pub fn test_comkey(n: usize) -> (Duration, Duration, Duration, Duration, Duratio
 // --------------------
 // Optimized scheme
 // --------------------
-pub fn test_optimized(n: usize) -> (Duration, Duration, Duration, Duration, Duration) {
+pub fn test_optimized(n: usize) {
     // Initialize servers
 	let moderator = o::Moderator::new();
 
-	let mut servers: Vec<Server> = Vec::with_capacity(n);
-	let mut pks: Vec<PublicKey> = Vec::with_capacity(n);
+	let mut servers: Vec<o::Server> = Vec::new();
+	let mut pks: Vec<PublicKey> = Vec::new();
 
 	// Collect server public keys
 	pks.push(moderator.get_pk());
 	for i in 1..n {
-		let si = Server::new();
+		let si = o::Server::new();
 		servers.push(si);
 		pks.push(servers[i-1].get_pk());
 	}
 
 	// Initialize senders and receivers
-    let mut senders: Vec<o::Client> = Vec::with_capacity(N);
-    for _i in 0..N {
-        let k_r = Aes256Gcm::generate_key(aes_gcm::aead::OsRng);
-        let sender = o::Client::new(k_r, pks.clone());
-        senders.push(sender);
-    }
+	let k_r = Aes256Gcm::generate_key(aes_gcm::aead::OsRng);
 
-	// Send a message
-    let mut ms: Vec<String> = Vec::with_capacity(N);
-    for _i in 0..N {
-        let m = Alphanumeric.sample_string(&mut rand::thread_rng(), 20);
-        ms.push(m);
-    }
+	let sender = o::Client::new(k_r, pks.clone());
+
+	// Send a message!
+	let m = "test message";
 
 	// Sender
-    let mut ctc2s: Vec<(Vec<u8>, Vec<u8>)> = Vec::with_capacity(N);
-    let now = Instant::now();
-    for i in 0..N {
-        let (ct, c2) = o::Client::send(&ms[i], senders[i].k_r, &pks, n);
-        ctc2s.push((ct, c2));
-    }
-    let t_send = now.elapsed();
+	let (mut ct, c2) = o::Client::send(m, sender.k_r, &pks, n);
 
 	// Moderator
-    let mut ctxs: Vec<String> = Vec::with_capacity(N);
-    let mut sigmas: Vec<Vec<u8>> = Vec::with_capacity(N);
-    let mut sigma_cs: Vec<Vec<u8>> = Vec::with_capacity(N);
-    let mut mrts: Vec<Vec<u8>> = Vec::with_capacity(N);
-    let now = Instant::now();
-    for i in 0..N {
-        let (_, c2) = ctc2s[i].clone();
-
-        let ctx = Alphanumeric.sample_string(&mut rand::thread_rng(), CTX_LEN);
-        ctxs.push(ctx.clone());
-
-        let (sigma, sigma_c) = o::Moderator::mod_process(&moderator.k_m, &c2, &ctx);
-        sigmas.push(sigma.clone());
-        sigma_cs.push(sigma_c.clone());
-
-        let mrt = bincode::serialize(&(c2.clone(), ctx, sigma, sigma_c)).unwrap();
-
-        mrts.push(mrt);
-    }
-    let t_mod_process = now.elapsed();
-
-    let mut sts: Vec<(Vec<u8>, Vec<u8>)> = Vec::with_capacity(N);
-    let now = Instant::now();
-    for i in 0..N {
-        let (_, _, c3) = c1c2c3s[i].clone();
-        let mrt = mrts[i].clone();
-
-        let mut st = (c3, mrt);
-        st = o::Server::process(&moderator.sk, st);
-        sts.push(st.clone());
-    }
-    let mut t_process = now.elapsed();
-
-    println!("mrt size general: {:?}", sts[0].1.len());
-
-    for i in 0..N {
-        let ct = cts[i].clone();
-        cts[i] = onion_peel(&moderator.sk, ct);
-    }
+	let ctx = "10char str";
+	let (sigma, sigma_c) = o::Moderator::mod_process(&moderator.k_m, &c2, ctx);
+	let mut st = bincode::serialize(&(c2.clone(), ctx, sigma, sigma_c)).unwrap();
+    println!("mrt size optimized: {:?}", st.len());
+	(ct, st) = o::Server::process(&moderator.sk, ct, st);
 
 	// Other servers
-    let now = Instant::now();
-    for i in 0..N {
-        let mut st = sts[i].clone();
-        for j in 1..n {
-            let sj = &servers[j-1];
-            st = o::Server::process(&sj.sk, st.clone());
-        }
-        sts[i] = st;
-    }
-    t_process = t_process + now.elapsed();
-
-    for i in 0..N {
-        let mut ct = cts[i].clone();
-        for j in 1..n {
-            let sj = &servers[j-1];
-            ct = onion_peel(&sj.sk, ct.to_vec());
-        }
-        cts[i] = ct;
-    }
+	for i in 1..n {
+		let si = &servers[i-1];
+		(ct, st) = o::Server::process(&si.sk, ct, st);
+	}
 
 	// Receiver
-    let mut reports: Vec<(String, String, (Vec<u8>, Vec<u8>), Vec<u8>)> = Vec::with_capacity(N);
-    let now = Instant::now();
-    for i in 0..N {
-        let k_r = senders[i].k_r;
-        let ct = cts[i].clone();
-        let st = sts[i].clone();
-	    let (m, ctx, rd, sigma) = o::Client::read(k_r, ct, st, n);
-        reports.push((m, ctx, rd, sigma));
-    }
-    let t_read = now.elapsed();
+	let (m, ctx, rd, sigma) = o::Client::read(k_r, ct, st, n);
 
 	// Reporting back to moderator
-    let now = Instant::now();
-    for i in 0..N {
-        let (m, ctx, rd, sigma) = reports[i].clone();
-        let res = o::Moderator::moderate(&moderator.k_m, &m, &ctx, rd, sigma);
-        if !res {
-            panic!("Report failed");
-        }
-    }
-    let t_moderate = now.elapsed();
+	let res = o::Moderator::moderate(&moderator.k_m, &m, &ctx, rd, sigma);
 
-    (t_send, t_mod_process, t_process, t_read, t_moderate)
+	if res {
+		println!("Report success!");
+	} else {
+		println!("Report failed");
+	}
 }
