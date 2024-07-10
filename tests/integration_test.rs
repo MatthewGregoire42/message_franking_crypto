8d9950048bd0a248adab2d0c82a81a267df442d6
Matthew Gregoire <mattyg@live.unc.edu>
2024-07-10 17:30:28 -0400

    Added tests

--- tests/integration_test.rs
+++ tests/integration_test.rs
@@ -1,214 +1,267 @@
-// use message_franking_crypto::franking::*;
-// use curve25519_dalek::scalar::Scalar;
-// use curve25519_dalek::ristretto::RistrettoPoint;
-// use rand::prelude::*;
-// use generic_array::GenericArray;
-// use rand_chacha::ChaCha8Rng;
-// use hmac::{Hmac, Mac, NewMac};
-// use rand_chacha::rand_core::SeedableRng;
-// use aes_gcm::{
-//     aead::{Aead, AeadCore, KeyInit},
-//     Aes256Gcm, Key
-// };
-// use sha2::{Sha256, Digest};
+extern crate zkp;
+use message_franking_crypto::lib_common::*;
+use message_franking_crypto::lib_common::{onion_peel, onion_encrypt};
+use message_franking_crypto::lib_general as g;
+use message_franking_crypto::lib_trap as t;
+use message_franking_crypto::lib_comkey as c;
+use message_franking_crypto::lib_optimized as o;
+use message_franking_crypto::lib_plain as p;
+use crypto_box::PublicKey;
+use aes_gcm::{
+    aead::KeyInit,
+    Aes256Gcm
+};
 
+#[test]
+fn test_general() {
 
-// #[test]
-// fn send_reencrypt() {
-//     let mut seed: <ChaCha8Rng as SeedableRng>::Seed = Default::default();
-//     thread_rng().fill(&mut seed);
-//     let rng = ChaCha8Rng::from_seed(seed);
-//     let k_r = Aes256Gcm::generate_key(rng);
-//     let cipher = Aes256Gcm::new(&k_r);
+     // Initialize servers
+	let moderator = g::Moderator::new();
 
-//     // Create Server PK
-//     let mut rng_el = rand::thread_rng();
-//     let x = Scalar::random(&mut rng_el);
-//     let g: &RistrettoPoint = &GEN_G;
-//     let pk = g * x;
+	let mut servers: Vec<Server> = Vec::with_capacity(9);
+	let mut pks: Vec<PublicKey> = Vec::with_capacity(10);
 
-//     // Server PK is the same as moderator PK in this case 
-//     let pk_m = pk.clone();
+	// Collect server public keys
+	pks.push(moderator.get_pk());
+	for i in 1..10 {
+		let si = Server::new();
+		servers.push(si);
+		pks.push(servers[i-1].get_pk());
+	}
 
-//     // Call send_reencrypt
-//     let ((c1, c2), (c1_m, c2_m)) = Client::send_reencrypt("Hello, World!", k_r, pk, pk_m);
+    // Sender
+    let k_r = Aes256Gcm::generate_key(aes_gcm::aead::OsRng);
 
-//     // Decrypt the first ciphertext and ensure that everything is ok:
-//     let s = c1 * x;
-//     let mut hasher1 = Sha256::new();
-//     hasher1.update(s.compress().to_bytes());
-//     let mut hash_s: Vec<u8> = hasher1.clone().finalize().to_vec();
-//     let zero_vec = vec![0; c2.len() - 32];
-//     hash_s.extend(zero_vec);
-//     let ct: Vec<u8> = c2
-//             .iter()
-//             .zip(hash_s.iter())
-//             .map(|(&x1, &x2)| x1 ^ x2)
-//             .collect();
-//     let nonce: &GenericArray<u8, aes_gcm::aead::consts::U12> = GenericArray::from_slice(&ct[0..12]);
-//     let plaintext_pkg = cipher.decrypt(nonce, &ct[12..]).unwrap();
-//     let plainttext: MessagePackage = bincode::deserialize(&plaintext_pkg).unwrap();
+    let (s, rs, c3) = g::Client::send_preprocessing(&pks, 10);
+    let (c1, c2) = g::Client::send_online("test message", k_r, s, rs);
+    let mut ct = onion_encrypt(pks, c1);
 
-//     assert_eq!(plainttext.m, "Hello, World!");
+    // Moderator
+    let ctx = "message co";
+    let (sigma, sigma_c) = g::Moderator::mod_process(&moderator.k_m, &c2, ctx);
+    let mrt = bincode::serialize(&(c2.clone(), ctx, sigma, sigma_c)).unwrap();
 
-//     // Decrypt the second ciphertext and ensure that everything is ok: 
-//     let s = c1_m * x;
-//     let mut hasher2 = hasher1.clone();
-//     hasher2.update(s.compress().to_bytes());
-//     let mut hash_s: Vec<u8> = hasher2.finalize().to_vec();
-//     let zero_vec = vec![0; c2.len() - 32];
-//     hash_s.extend(zero_vec);
-//     let k_tf: Vec<u8> = c2_m
-//             .iter()
-//             .zip(hash_s.iter())
-//             .map(|(&x1, &x2)| x1 ^ x2)
-//             .collect();
-//     let mod_package: ModeratorPackage = bincode::deserialize(&k_tf).unwrap();
+    let mut st = (c3, mrt);
+    st = Server::process(&moderator.sk, st);
+    ct = onion_peel(&moderator.sk, ct.to_vec());
 
-//     assert_eq!(plainttext.tf, mod_package.tf);
-// }
+    // Other servers
+    for i in 1..10 {
+        println!("{}", i);
+        st = Server::process(&servers[i-1].sk, st);
+        ct = onion_peel(&servers[i-1].sk, ct.to_vec());
+    }
 
-// #[test]
-// fn receive_reencrypt_rejects_incorrect_message() {
+    // Receiver
+    let (m, ctx, rd, sigma) = g::Client::read(k_r, ct, st, 10);
 
-//     let message = "Hello, World!";
-//     // Choose random seed s and seed the RNG
-//     // ====================================================================
-//     let mut seed: <ChaCha8Rng as SeedableRng>::Seed = Default::default();
-//     thread_rng().fill(&mut seed);
-//     let mut rng = ChaCha8Rng::from_seed(seed);
-//     // ====================================================================
+    // Moderator
+    let res = g::Moderator::moderate(&moderator.k_m, &m, &ctx, rd.clone(), sigma.clone());
 
-//     // Generate moderator key k and receiver key k_r
-//     // ====================================================================
-//     let k = Aes256Gcm::generate_key(&mut rng);
-//     let k_r = Aes256Gcm::generate_key(&mut rng);
-//     // ====================================================================
+    // Valid franking works
+    assert!(res);
 
-//     // Generate ephemeral MAC key kf
-//     // ====================================================================
-//     type HmacSha256 = Hmac<Sha256>;
-//     let mackey_kf = thread_rng().gen::<[u8; 32]>();
-//     // ====================================================================
+    let res = g::Moderator::moderate(&moderator.k_m, &m, "some other", rd, sigma);
+    assert!(!res);
+}
 
-//     // Compute franking tag tf 
-//     // ====================================================================
-//     let mut mac = HmacSha256::new_varkey(&mackey_kf).expect("HMAC");
-//     let macinput: &[u8] = message.as_bytes();
-//     mac.update(macinput);
-//     let franktag_tf = (mac.finalize()).into_bytes();
-//     // ====================================================================
+#[test]
+fn test_comkey() {
 
-//     // Encrypt m, kf, tf, s --> ct using AES-GCM
-//     // ====================================================================
-//     let zero_vec = vec![0; 32];
-//     let msg_package = MessagePackage {
-//         m: message,
-//         kf: mackey_kf,
-//         tf: zero_vec.clone(),
-//         s: seed,
-//     };
-//     let mut buffer: Vec<u8> = Vec::new();
-//     buffer.extend_from_slice(&bincode::serialize(&msg_package).unwrap());
-//     let cipher = Aes256Gcm::new(&k_r);
-//     let nonce = Aes256Gcm::generate_nonce(&mut rng);
-//     let ciphertext = cipher.encrypt(&nonce, buffer.as_ref()).unwrap();
-//     let mut ct: Vec<u8> = Vec::new();
-//     ct.extend_from_slice(&nonce);
-//     ct.extend_from_slice(&ciphertext);
-//     // ====================================================================
+    // Initialize servers
+   let moderator = c::Moderator::new();
 
+   let mut servers: Vec<Server> = Vec::with_capacity(9);
+   let mut pks: Vec<PublicKey> = Vec::with_capacity(10);
 
-//     // Encrypt tf, tr, ctxt under k using AES-GCM
-//     // ====================================================================
-//     let rep_package = ReceiverReportPackage {
-//         tr: zero_vec.clone(),
-//         tf: zero_vec.clone(),
-//         ctxt: zero_vec.clone(),
-//     };
-//     let mut buffer: Vec<u8> = Vec::new();
-//     buffer.extend_from_slice(&bincode::serialize(&rep_package).unwrap());
-//     let cipher = Aes256Gcm::new(&k);
-//     let nonce = Aes256Gcm::generate_nonce(&mut rng);
-//     let ciphertext = cipher.encrypt(&nonce, buffer.as_ref()).unwrap();
-//     let mut ct_rep: Vec<u8> = Vec::new();
-//     ct_rep.extend_from_slice(&nonce);
-//     ct_rep.extend_from_slice(&ciphertext);   
-//     // ====================================================================
+   // Collect server public keys
+   pks.push(moderator.get_pk());
+   for i in 1..10 {
+       let si = Server::new();
+       servers.push(si);
+       pks.push(servers[i-1].get_pk());
+   }
 
-//     let plainttext = Client::receive_reencrypt(k_r, ct, ct_rep);
-//     assert!(plainttext.is_err());
-// }
+   // Sender
+   let k_r = Aes256Gcm::generate_key(aes_gcm::aead::OsRng);
+   let receiver = c::Client::new(k_r, pks.clone(), moderator.sigma_k.compress());
 
-// #[test]
-// fn receive_reencrypt_decrypts_correct_message() {
+   let (s, rs, c3) = c::Client::send_preprocessing(&pks, 10);
+   let (c1, c2) = c::Client::send_online("test message", k_r, s, rs);
+   let mut ct = onion_encrypt(pks, c1);
 
-//     let message = "Hello, World!";
-//     // Choose random seed s and seed the RNG
-//     // ====================================================================
-//     let mut seed: <ChaCha8Rng as SeedableRng>::Seed = Default::default();
-//     thread_rng().fill(&mut seed);
-//     let mut rng = ChaCha8Rng::from_seed(seed);
-//     // ====================================================================
+   // Moderator
+   let ctx = "message co";
+   let (sigma, sigma_c) = moderator.mod_process(&moderator.k_m, &c2, ctx);
+   let mrt = bincode::serialize(&(c2.clone(), ctx, sigma, sigma_c)).unwrap();
 
-//     // Generate moderator key k and receiver key k_r
-//     // ====================================================================
-//     let k = Aes256Gcm::generate_key(&mut rng);
-//     let k_r = Aes256Gcm::generate_key(&mut rng);
-//     // ====================================================================
+   let mut st = (c3, mrt);
+   st = Server::process(&moderator.sk, st);
+   ct = onion_peel(&moderator.sk, ct.to_vec());
 
-//     // Generate ephemeral MAC key kf
-//     // ====================================================================
-//     type HmacSha256 = Hmac<Sha256>;
-//     let mackey_kf = thread_rng().gen::<[u8; 32]>();
-//     // ====================================================================
+   // Other servers
+   for i in 1..10 {
+       println!("{}", i);
+       st = Server::process(&servers[i-1].sk, st);
+       ct = onion_peel(&servers[i-1].sk, ct.to_vec());
+   }
 
-//     // Compute franking tag tf 
-//     // ====================================================================
-//     let mut mac = HmacSha256::new_varkey(&mackey_kf).expect("HMAC");
-//     let macinput: &[u8] = message.as_bytes();
-//     mac.update(macinput);
-//     let franktag_tf = (mac.finalize()).into_bytes();
-//     // ====================================================================
+   // Receiver
+   let (m_prime, ctx_prime, rd_prime, sigma_prime) = receiver.read(k_r, ct, st, 10);
 
-//     // Encrypt m, kf, tf, s --> ct using AES-GCM
-//     // ====================================================================
-//     let msg_package = MessagePackage {
-//         m: message,
-//         kf: mackey_kf,
-//         tf: franktag_tf.to_vec(),
-//         s: seed,
-//     };
-//     let mut buffer: Vec<u8> = Vec::new();
-//     buffer.extend_from_slice(&bincode::serialize(&msg_package).unwrap());
-//     let cipher = Aes256Gcm::new(&k_r);
-//     let nonce = Aes256Gcm::generate_nonce(&mut rng);
-//     let ciphertext = cipher.encrypt(&nonce, buffer.as_ref()).unwrap();
-//     let mut ct: Vec<u8> = Vec::new();
-//     ct.extend_from_slice(&nonce);
-//     ct.extend_from_slice(&ciphertext);
-//     // ====================================================================
+   assert_eq!("test message", m_prime);
+   assert_eq!(ctx, ctx_prime);
 
+   // Moderator
+   let res = c::Moderator::moderate(&moderator.k_m, &m_prime, &ctx_prime, rd_prime.clone(), sigma_prime.clone());
 
-//     // Encrypt tf, tr, ctxt under k using AES-GCM
-//     // ====================================================================
-//     let zero_vec = vec![0; 32];
-//     let rep_package = ReceiverReportPackage {
-//         tr: zero_vec.clone(),
-//         tf: franktag_tf.to_vec(),
-//         ctxt: zero_vec.clone(),
-//     };
-//     let mut buffer: Vec<u8> = Vec::new();
-//     buffer.extend_from_slice(&bincode::serialize(&rep_package).unwrap());
-//     let cipher = Aes256Gcm::new(&k);
-//     let nonce = Aes256Gcm::generate_nonce(&mut rng);
-//     let ciphertext = cipher.encrypt(&nonce, buffer.as_ref()).unwrap();
-//     let mut ct_rep: Vec<u8> = Vec::new();
-//     ct_rep.extend_from_slice(&nonce);
-//     ct_rep.extend_from_slice(&ciphertext);   
-//     // ====================================================================
+   // Valid franking works
+   assert!(res);
 
-//     let plainttext = Client::receive_reencrypt(k_r, ct, ct_rep);
-//     assert!(plainttext.is_ok());
-//     assert_eq!(plainttext.unwrap().m, String::from(message));
-// }
+   let res = c::Moderator::moderate(&moderator.k_m, &m_prime, "some other", rd_prime, sigma_prime);
+   assert!(!res);
+}
+
+#[test]
+fn test_trap() {
+
+    // Initialize servers
+   let moderator = t::Moderator::new();
+
+   let mut servers: Vec<Server> = Vec::with_capacity(9);
+   let mut pks: Vec<PublicKey> = Vec::with_capacity(10);
+
+   // Collect server public keys
+   pks.push(moderator.get_pk());
+   for i in 1..10 {
+       let si = Server::new();
+       servers.push(si);
+       pks.push(servers[i-1].get_pk());
+   }
+
+   // Sender
+   let k_r = Aes256Gcm::generate_key(aes_gcm::aead::OsRng);
+
+   let (s, rs, c3) = t::Client::send_preprocessing(&pks, 10, 5);
+   let (c1, c2) = t::Client::send_online("test message", k_r, s, rs, 10, 5);
+   let mut ct = onion_encrypt(pks, c1);
+
+   // Moderator
+   let ctx = "message co";
+   let (sigma, sigma_c) = t::Moderator::mod_process(&moderator.k_m, &c2, ctx, 5);
+   let mrt = bincode::serialize(&(c2.clone(), ctx, sigma, sigma_c)).unwrap();
+
+   let mut st = (c3, mrt);
+   st = Server::process(&moderator.sk, st);
+   ct = onion_peel(&moderator.sk, ct.to_vec());
+
+   // Other servers
+   for i in 1..10 {
+       println!("{}", i);
+       st = Server::process(&servers[i-1].sk, st);
+       ct = onion_peel(&servers[i-1].sk, ct.to_vec());
+   }
+
+   // Receiver
+   let reports = t::Client::read(k_r, ct, st, 10, 5);
+
+   for i in 0..5 {
+    let (m_prime, ctx_prime, rd_prime, sigma_prime) = reports[i].clone();
+
+    if i == 0 {
+        assert_eq!("test message", m_prime);
+    } else {
+        assert_eq!("", m_prime);
+    }
+
+    let res = t::Moderator::moderate(&moderator.k_m, &m_prime, &ctx_prime, rd_prime.clone(), sigma_prime.clone());
+
+    assert!(res);
+
+    let res = t::Moderator::moderate(&moderator.k_m, &m_prime, "some other", rd_prime, sigma_prime);
+    assert!(!res);
+   }
+}
+
+#[test]
+fn test_optimized() {
+
+    // Initialize servers
+   let moderator = o::Moderator::new();
+
+   let mut servers: Vec<Server> = Vec::with_capacity(9);
+   let mut pks: Vec<PublicKey> = Vec::with_capacity(10);
+
+   // Collect server public keys
+   pks.push(moderator.get_pk());
+   for i in 1..10 {
+       let si = Server::new();
+       servers.push(si);
+       pks.push(servers[i-1].get_pk());
+   }
+
+   // Sender
+   let k_r = Aes256Gcm::generate_key(aes_gcm::aead::OsRng);
+
+   let (mut ct, c2) = o::Client::send("test message",k_r, &pks, 10);
+
+   // Moderator
+   let ctx = "message co";
+   let (sigma, sigma_c) = o::Moderator::mod_process(&moderator.k_m, &c2, ctx);
+   let mut mrt = bincode::serialize(&(c2.clone(), ctx, sigma, sigma_c)).unwrap();
+
+   (ct, mrt) = o::Server::process(&moderator.sk, ct, mrt);
+
+   // Other servers
+   for i in 1..10 {
+       println!("{}", i);
+       (ct, mrt) = o::Server::process(&servers[i-1].sk, ct, mrt);
+   }
+
+   // Receiver
+   let (m_prime, ctx_prime, rd_prime, sigma_prime) = o::Client::read(k_r, ct, mrt, 10);
+
+   assert_eq!("test message", m_prime);
+   assert_eq!(ctx, ctx_prime);
+
+   // Moderator
+   let res = o::Moderator::moderate(&moderator.k_m, &m_prime, &ctx_prime, rd_prime.clone(), sigma_prime.clone());
+
+   // Valid franking works
+   assert!(res);
+
+   let res = o::Moderator::moderate(&moderator.k_m, &m_prime, "some other", rd_prime, sigma_prime);
+   assert!(!res);
+}
+
+#[test]
+fn test_plain() {
+
+     // Initialize servers
+	let moderator = p::Moderator::new();
+
+    // Sender
+    let k_r = Aes256Gcm::generate_key(aes_gcm::aead::OsRng);
+
+    let (c1, c2) = p::Client::send("test message", k_r);
+
+    // Moderator
+    let ctx = "message co";
+    let sigma = p::Moderator::mod_process(&moderator.k_m, &c2, ctx);
+
+    // Receiver
+    let (m_prime, ctx_prime, rd_prime, sigma_prime) = 
+        p::Client::read(k_r, c1, (c2, ctx.to_string(), sigma));
+
+    assert_eq!("test message", m_prime);
+    assert_eq!(ctx, ctx_prime);
+
+    // Moderator
+    let res = p::Moderator::moderate(&moderator.k_m, &m_prime, &ctx_prime, rd_prime.clone(), sigma_prime.clone());
+
+    // Valid franking works
+    assert!(res);
+
+    let res = p::Moderator::moderate(&moderator.k_m, &m_prime, "some other", rd_prime, sigma_prime);
+    assert!(!res);
+}
